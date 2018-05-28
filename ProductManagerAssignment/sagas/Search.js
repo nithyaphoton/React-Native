cbbd2f4e7d6c21ff837fe84a6c07909bf2d26527
nithyaphoton <39086207+nithyaphoton@users.noreply.github.com>
2018-05-28 20:50:23 +0530

    Search item updated

---
+++ ProductManagerAssignment/sagas/Search.js
@@ -0,0 +1,23 @@
+import {
+    put,
+    takeLatest
+} from "redux-saga/effects";
+import * as actionCreators from "../actionCreators/SearchItem"
+import {
+    GET_PRODUCTS_FROM_SEARCH
+} from "../actionTypes/SearchItem";
+
+let URI = "http://172.16.105.165:4000";
+
+function* getProductsFromSearch(action) {
+    try {
+        let products = yield fetch(`${URI}/products?q=${action.searchText}&_page=${action.page}&_limit=${action.limit}`).then(r => r.json());
+        yield put(actionCreators.getSearchProductsSuccess(products))
+    } catch (error) {
+        yield put(actionCreators.getSearchProductsFailure(error))
+    }
+}
+
+export function* productSearchWatchers() {
+    yield takeLatest(GET_PRODUCTS_FROM_SEARCH, getProductsFromSearch)
+}
