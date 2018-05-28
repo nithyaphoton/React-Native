eb18447bcf2767f1415e69bb48238615e437b10e
nithyaphoton <39086207+nithyaphoton@users.noreply.github.com>
2018-05-28 20:40:27 +0530

    delete reducers are added

--- ProductManagerAssignment/reducers/index.js
+++ ProductManagerAssignment/reducers/index.js
@@ -2,7 +2,10 @@ import {
     combineReducers
 } from "redux";
 import productReducer from "./product";
-import storeReducer from "./store"
+import storeReducer from "./store";
+import searchReducer from './search';
+import addProductReducer from "./addProduct";
+import productDetailReducer from './productDetail';
 import {
     createNavigationReducer
 } from "react-navigation-redux-helpers";
@@ -14,7 +17,10 @@ const navReducer = createNavigationReducer(AppNavigator);
 const rootReducer = combineReducers({
     productState: productReducer,
     storeState: storeReducer,
-    navState: navReducer
+    navState: navReducer,
+     addProductState: addProductReducer,
+    productDetailState: productDetailReducer,
+     searchState: searchReducer
 })
 
 export default rootReducer;
